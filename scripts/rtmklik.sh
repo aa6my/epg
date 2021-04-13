879b4ee00f40e17b155ab94920765cfd25d60cbb
pi@raspberrypi <tanyuxuan2005@gmail.com>
2021-04-13 22:00:35 +0800

    Scripts added without option to upload into GitHub

---
+++ scripts/rtmklik.sh
@@ -0,0 +1,8 @@
+cd /home/pi/.wg++
+cp /home/pi/.wg++/deploy/webgrab-configs/rtmklik.config.xml WebGrab++.config.xml
+sudo ./run.sh
+mv guide.xml /home/pi/Desktop/guide.xml
+
+echo "The XML file is now located on your desktop."
+echo "Modify this file by editing it - change the file path at mv /home/pi/Desktop to your desired file path."
+
