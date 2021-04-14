12d179d627c39001548c9dfc032c1313574156cf
tanyu@DESKTOP-HCNFE8E <tanyuxuan2005@gmail.com>
2021-04-14 20:38:20 +0800

    Deleted wrongly-placed files

--- scripts/windows/GenerateAstro.bat
+++ scripts/windows/GenerateAstro.bat
@@ -1,29 +1,18 @@
-::Windows Global Variables
 @echo off
-::Set Variables (Admin required!)
-dateadd_mod 7
-set /p 7dtext= <7d.txt
-
-:: Start generating EPG for Astro
-:: Changing the directory from external HDD to Local Disk
 cd %USERPROFILE%\AppData\Local\WebGrab+Plus
 C:
-:: Copying config files from EHDD to WebGrab Directory
 del "WebGrab++.config.xml"
 copy G:\git-working\epg\webgrab-configs\astro.config.xml "WebGrab++.config.xml"
-:: Start Generating
 Run.lnk
-:: Post-generation action
-:::: Delete original astro.xml
 del G:\git-working\epg\astro.xml /Q
-:::: Change Local Disk to EHDD
-:::: Copy generated astro.xml to git directory
-copy "%USERPROFILE%\AppData\Local\WebGrab+Plus\astro.xml" astro.xml
-:: Push into repository
+cd G:\git-working\epg
+G:
+copy "%USERPROFILE%\AppData\Local\WebGrab+Plus\astro.xml" G:\git-working\epg\astro.xml
 git add astro.xml
-git commit -m "Astro EPG from %date:~4,11% to %7dtext%/ Generated at %date:~4,11% %date:~0,3% %time:~0,2%:%time:~3,2%:%time:~6,2% MYT"
+git commit -m "Astro EPG from %date:~4,11% (7 Days) Generated at %date:~4,11% %date:~0,3% %time:~0,2%:%time:~3,2%:%time:~6,2% MYT"
 git push
-echo "Generated successfully."
-echo "Website: astro.com.my"
-echo "Name: Astro"
-echo "Commit Name: Astro EPG from %date:~4,11% to %7dtext%/ Generated at %date:~4,11% %date:~0,3% %time:~0,2%:%time:~3,2%:%time:~6,2% MYT
+del 7d.txt /Q
+echo Generated successfully.
+echo Website: astro.com.my
+echo Name: Astro
+echo Astro EPG from %date:~4,11% (7 Days) Generated at %date:~4,11% %date:~0,3% %time:~0,2%:%time:~3,2%:%time:~6,2% MYT
--- scripts/windows/GenerateRTMKlik.bat
+++ scripts/windows/GenerateRTMKlik.bat
@@ -1,19 +1,16 @@
-@echo off
-del 7d.txt /Q
-dateadd_mod 7
-set /p 7dtext= <7d.txt
 cd %USERPROFILE%\AppData\Local\WebGrab+Plus
 C:
 del "WebGrab++.config.xml"
 copy G:\git-working\epg\webgrab-configs\rtmklik.config.xml "WebGrab++.config.xml"
 Run.lnk
 del G:\git-working\epg\rtmklik.xml /Q
+G:
 copy "%USERPROFILE%\AppData\Local\WebGrab+Plus\rtmklik.xml" rtmklik.xml
 git add rtmklik.xml
-git commit -m "rtmklik EPG from %date:~4,11% to %7dtext%/ Generated at %date:~4,11% %date:~0,3% %time:~0,2%:%time:~3,2%:%time:~6,2% MYT"
+git commit -m "rtmklik EPG from %date:~4,11% (7 Days) Generated at %date:~4,11% %date:~0,3% %time:~0,2%:%time:~3,2%:%time:~6,2% MYT"
 git push
 del 7d.txt /Q
-echo "Generated successfully."
-echo "Website: rtmklik.rtm.gov.my"
-echo "Name: rtmklik"
-echo "Commit Name: rtmklik EPG from %date:~4,11% to %7dtext%/ Generated at %date:~4,11% %date:~0,3% %time:~0,2%:%time:~3,2%:%time:~6,2% MYT
+echo Generated successfully.
+echo Website: rtmklik.rtm.gov.my
+echo Name: rtmklik
+echo rtmklik EPG from %date:~4,11% (7 Days) Generated at %date:~4,11% %date:~0,3% %time:~0,2%:%time:~3,2%:%time:~6,2% MYT
