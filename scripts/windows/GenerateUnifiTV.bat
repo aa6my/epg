5e28f2a9b7817808f4d03572e2fe633a80b38500
tanyu@DESKTOP-HCNFE8E <tanyuxuan2005@gmail.com>
2021-04-14 21:42:00 +0800

    Added script for generating unifi TV EPG

---
+++ scripts/windows/GenerateUnifiTV.bat
@@ -0,0 +1,18 @@
+@echo off
+cd %USERPROFILE%\AppData\Local\WebGrab+Plus
+C:
+del "WebGrab++.config.xml"
+copy G:\git-working\epg\webgrab-configs\unifitv.config.xml "WebGrab++.config.xml"
+Run.lnk
+del G:\git-working\epg\unifitv.xml /Q
+cd G:\git-working\epg
+G:
+copy "%USERPROFILE%\AppData\Local\WebGrab+Plus\unifitv.xml" G:\git-working\epg\unifitv.xml
+git add unifitv.xml
+git commit -m "unifi TV EPG from %date:~4,11% (7 Days) Generated at %date:~4,11% %date:~0,3% %time:~0,2%:%time:~3,2%:%time:~6,2% MYT"
+git push
+del 7d.txt /Q
+echo Generated successfully.
+echo Website: Various
+echo Name: unifi TV
+echo unifitv EPG from %date:~4,11% (7 Days) Generated at %date:~4,11% %date:~0,3% %time:~0,2%:%time:~3,2%:%time:~6,2% MYT
