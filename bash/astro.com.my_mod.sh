d20436969d04c502eac83a5603a8cd2a1582bc0f
tanyu@DESKTOP-HCNFE8E <tanyuxuan2005@gmail.com>
2021-04-15 17:04:52 +0800

    Deleted bash folder

--- bash/astro.com.my_mod.sh
+++
@@ -1,7 +0,0 @@
-# Add modified site.ini for astro.com.my (Linux)
-# Clone weareblahs/epg repository (this is where the modified SiteIni comes from)
-git clone https://github.com/weareblahs/epg
-# Copying modified SiteIni from repository to WebGrab directory
-cp epg/modified-site-ini/astro.com.my.ini ~/.wg++/siteini.pack/Malaysia/astro.com.my.ini
-# Delete the repository from your computer
-# rm -rf epg/*
